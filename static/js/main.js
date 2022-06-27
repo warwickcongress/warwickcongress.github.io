a784d0f62e8b5559b37a0af22e71a6225efc55d1
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-06-27 16:45:25 +0100

    Rearranged img folder

--- static/js/main.js
+++ static/js/main.js
@@ -11,9 +11,9 @@ container.addEventListener('scroll', function() {
     title.classList.toggle('dark', container.scrollTop > 0 && $('#contact').offset().top != 0)
 
     if (container.scrollTop > 0 && $('#contact').offset().top != 0) {
-        document.getElementById("warcon-logo").src = "static/img/logo-dark.png";
+        document.getElementById("warcon-logo").src = "static/img/logos/logo-dark.png";
     } else {
-        document.getElementById("warcon-logo").src = "static/img/logo-light.png";
+        document.getElementById("warcon-logo").src = "static/img/logos/logo-light.png";
     }
 
     let navLinks = document.getElementsByClassName("nav-link");
@@ -25,17 +25,17 @@ container.addEventListener('scroll', function() {
     title.classList.add('hide');
 
     // Change title based on current section
-    if ($('#about').offset().top == 0) {
-        title.innerHTML = "About";
-    } else if ($('#conferences').offset().top == 0) {
-        title.innerHTML = "Conferences";
-    } else if ($('#team').offset().top == 0) {
-        title.innerHTML = "Our Team";
-    } else if ($('#contact').offset().top == 0) {
-        title.innerHTML = "Contact Us";
-    } else {
-        title.innerHTML = " ";
-    }
+    // if ($('#about').offset().top == 0) {
+    //     title.innerHTML = "Warwick Congress";
+    // } else if ($('#conferences').offset().top == 0) {
+    //     title.innerHTML = "Conferences";
+    // } else if ($('#team').offset().top == 0) {
+    //     title.innerHTML = "Our Team";
+    // } else if ($('#contact').offset().top == 0) {
+    //     title.innerHTML = "Contact Us";
+    // } else {
+    //     title.innerHTML = " ";
+    // }
 
     // DOESN'T WORK
     title.classList.remove('hide');
