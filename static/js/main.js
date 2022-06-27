753bc30b7600e86dcd47b90c37f9e2b7a6a83b65
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-06-27 18:35:46 +0100

    Update index.html

--- static/js/main.js
+++ static/js/main.js
@@ -3,40 +3,26 @@
 
 // THIS IS HORRENDOUS CODE - TIDY IT UP NEIL
 // stick to either jQuery or JS, be consistent
-let container = document.querySelector('.container');
+var container = document.querySelector('.container');
+var title = document.querySelector('.page-title');
+var logoDark = document.querySelector("#logo-dark");
+var navlinks = document.querySelectorAll(".nav-link");
+var about = document.querySelector('#about')
+var contact = document.querySelector('#contact')
+
 container.addEventListener('scroll', function() {
 
-    let title = document.querySelector('.page-title');
-
-    title.classList.toggle('dark', container.scrollTop > 0 && $('#contact').offset().top != 0)
-
-    if (container.scrollTop > 0 && $('#contact').offset().top != 0) {
-        document.getElementById("warcon-logo").src = "static/img/logos/logo-dark.png";
+    if ((about.getBoundingClientRect().top <= 0) && (contact.getBoundingClientRect().top != 0)) {
+        logoDark.classList.remove('hidden');
+        title.classList.add('dark');
+        title.classList.remove('hidden');
+        for (i = 0; i < navlinks.length; i++)
+            navlinks[i].classList.add('dark');
     } else {
-        document.getElementById("warcon-logo").src = "static/img/logos/logo-light.png";
+        logoDark.classList.add('hidden');
+        title.classList.remove('dark');
+        title.classList.add('hidden');
+        for (i = 0; i < navlinks.length; i++)
+            navlinks[i].classList.remove('dark');
     }
-
-    let navLinks = document.getElementsByClassName("nav-link");
-    for (i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.toggle('dark', container.scrollTop > 0 && $('#contact').offset().top != 0);
-    }
-
-    // DOESN'T WORK
-    title.classList.add('hide');
-
-    // Change title based on current section
-    // if ($('#about').offset().top == 0) {
-    //     title.innerHTML = "Warwick Congress";
-    // } else if ($('#conferences').offset().top == 0) {
-    //     title.innerHTML = "Conferences";
-    // } else if ($('#team').offset().top == 0) {
-    //     title.innerHTML = "Our Team";
-    // } else if ($('#contact').offset().top == 0) {
-    //     title.innerHTML = "Contact Us";
-    // } else {
-    //     title.innerHTML = " ";
-    // }
-
-    // DOESN'T WORK
-    title.classList.remove('hide');
 })
