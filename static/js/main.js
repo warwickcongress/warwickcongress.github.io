82bad559e5dba46d82e3091083c982dc9b428dab
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-07-19 00:48:51 +0800

    Created new navbar

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,57 @@
+var navLinks = document.querySelectorAll('.nav-link');
+var navDropdown = document.querySelector('#nav-dropdown');
 
-var container = document.querySelector('.container');
-var logoDark  = document.querySelector("#logo-dark");
-var navlinks  = document.querySelectorAll(".nav-link");
-var about     = document.querySelector('#about')
-var contact   = document.querySelector('#contact')
-var copyright = document.querySelector('#copyright')
+// Change styling of logo, nav menu and copyright on scroll
+document.querySelector('.container').addEventListener('scroll', function() {
 
-// Change styling of logo and copyright
-container.addEventListener('scroll', function() {
+    var logoDark = document.querySelector("#logo-dark");
+    var aboutPage = document.querySelector('#about');
+    var confPage = document.querySelector('#conferences');
+    var teamPage = document.querySelector('#team');
+    var contactPage = document.querySelector('#contact');
+    var copyright = document.querySelector('#copyright');
 
-    if ((about.getBoundingClientRect().top <= 0) && (contact.getBoundingClientRect().top != 0)) {
+    // Show dark logo when not on home or contact page, otherwise show light logo
+    if ((aboutPage.getBoundingClientRect().top <= 0) && (contactPage.getBoundingClientRect().top != 0)) {
         logoDark.classList.remove('hidden');
-        for (i = 0; i < navlinks.length; i++)
-            navlinks[i].classList.add('dark');
     } else {
         logoDark.classList.add('hidden');
-        for (i = 0; i < navlinks.length; i++)
-            navlinks[i].classList.remove('dark');
+    }
+
+    // Hide nav dropdown menu
+    navDropdown.style.maxHeight = null;
+    
+    // Remove styling for current active nav link
+    document.querySelector(".active").classList.remove('active');
+
+    // Enable styling for active nav link
+    if (aboutPage.getBoundingClientRect().top > 0) {
+        document.querySelector("a[href='#home']").classList.add('active');
+    } else if ((aboutPage.getBoundingClientRect().top <= 0) && (confPage.getBoundingClientRect().top > 0)) {
+        document.querySelector("a[href='#about']").classList.add('active');
+    } else if ((confPage.getBoundingClientRect().top <= 0) && (teamPage.getBoundingClientRect().top > 0)) {
+        document.querySelector("a[href='#conferences']").classList.add('active');
+    } else if ((teamPage.getBoundingClientRect().top <= 0) && (contactPage.getBoundingClientRect().top > 0)) {
+        document.querySelector("a[href='#team']").classList.add('active');
+    } else if (contactPage.getBoundingClientRect().top <= 0) {
+        document.querySelector("a[href='#contact']").classList.add('active');
     }
 
     // Hide/show copyright on contact page
-    if (contact.getBoundingClientRect().top <= 0) {
+    if (contactPage.getBoundingClientRect().top <= 0) {
         copyright.classList.remove('hidden');
     } else {
         copyright.classList.add('hidden');
     }
-})
-
-var navLinks = document.querySelectorAll('.link');
+});
 
 // Show/hide nav menu
 for (i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('click', function() {
-        var panel = document.querySelector('#nav-panel');
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
+        if (navDropdown.style.maxHeight) {
+            navDropdown.style.maxHeight = null;
         } else {
-            panel.style.maxHeight = panel.scrollHeight + 'px';
+            navDropdown.style.maxHeight = navDropdown.scrollHeight + 'px';
         }
     });
 }
