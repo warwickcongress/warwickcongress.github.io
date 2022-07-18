6937d2b40f3b0b7d18478d88eda591f8e0a15862
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-07-18 18:29:56 +0800

    Removed conferences, fixed width for team section

--- static/js/main.js
+++ static/js/main.js
@@ -7,6 +7,7 @@ var about     = document.querySelector('#about')
 var contact   = document.querySelector('#contact')
 var copyright = document.querySelector('#copyright')
 
+// Change styling of logo, header and copyright on scroll
 container.addEventListener('scroll', function() {
 
     // Alter styling of header
@@ -30,52 +31,16 @@ container.addEventListener('scroll', function() {
     }
 })
 
+var navLinks = document.querySelectorAll('.link');
+
 // Show/hide nav menu
-function toggleAccordion() {
-    var panel = document.getElementsByClassName('accordion-panel')[0];
-    if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-    } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-    }
+for (i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function() {
+        var panel = document.querySelector('#nav-panel');
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
 }
-
-// Progress bar in conferences section
-/*
-$(document).ready(function(){
-    $(".step").click( function() {
-        $(this).addClass("active").prevAll().addClass("active");
-        $(this).nextAll().removeClass("active");
-    });
-
-    $(".step01").click( function() {
-        $("#line-progress").css("width", "16%");
-        $(".2022").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step02").click( function() {
-        $("#line-progress").css("width", "32%");
-        $(".2021").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step03").click( function() {
-        $("#line-progress").css("width", "48%");
-        $(".2020").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step04").click( function() {
-        $("#line-progress").css("width", "64%");
-        $(".2019").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step05").click( function() {
-        $("#line-progress").css("width", "80%");
-        $(".2018").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step06").click( function() {
-        $("#line-progress").css("width", "100%");
-        $(".2017").addClass("active").siblings().removeClass("active");
-    });
-});
-*/
