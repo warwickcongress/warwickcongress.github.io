4a70cc215736c1767d484597e0df5e8ce2c73f9d
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-07-24 22:09:01 +0800

    Removed copyright statement

--- static/js/main.js
+++ static/js/main.js
@@ -13,7 +13,7 @@ for (i = 0; i < navLinks.length; i++) {
 }
 
 
-// Change styling of logo, nav menu and copyright on scroll
+// Change styling of logo and nav menu on scroll
 document.querySelector('.container').addEventListener('scroll', function() {
 
     var logoDark = document.querySelector("#logo-dark");
@@ -21,7 +21,6 @@ document.querySelector('.container').addEventListener('scroll', function() {
     var confPage = document.querySelector('#conferences');
     var teamPage = document.querySelector('#team');
     var contactPage = document.querySelector('#contact');
-    var copyright = document.querySelector('#copyright');
 
     // Show dark logo when not on home or contact page, otherwise show light logo
     if ((aboutPage.getBoundingClientRect().top <= 0) && (contactPage.getBoundingClientRect().top != 0)) {
@@ -47,12 +46,5 @@ document.querySelector('.container').addEventListener('scroll', function() {
         document.querySelector("a[href='#team']").classList.add('active');
     } else if (contactPage.getBoundingClientRect().top <= 0) {
         document.querySelector("a[href='#contact']").classList.add('active');
-    }
-
-    // Hide/show copyright on contact page
-    if (contactPage.getBoundingClientRect().top <= 0) {
-        copyright.classList.remove('hidden');
-    } else {
-        copyright.classList.add('hidden');
     }
 });
